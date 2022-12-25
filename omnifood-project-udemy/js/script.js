e43252b5aadd728e991066299457da6312b50c59
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2022-12-25 12:12:31 +0200

    added scripts to fix safari scrolling smooth also fixed navgiation disappearing once using it

--- omnifood-project-udemy/js/script.js
+++ omnifood-project-udemy/js/script.js
@@ -36,13 +36,22 @@ allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    console.log(href);
 
     //scrollback to top
-    if (href === "#") window.scrollTo({
-      top: 0,
-      behavior: "smooth";
-    })
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
   });
 });
 
